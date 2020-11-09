96c5700d42bc6406ca59bd7db51a1cba3de4ea7e
Augusto Kraszczuk <61908423+AugustoKras@users.noreply.github.com>
2020-11-09 20:08:51 -0300

    Add files via upload

--- Verificador_de_idade/script.js
+++ Verificador_de_idade/script.js
@@ -8,7 +8,43 @@ function verificar() {
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        //res.innerHTML = `idade calculada: ${idade}`
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if(idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute('src','foto-bb-m.jpeg')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src','foto-jovem-m.jpeg')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src','foto-adulto-m.jpeg')
+            } else {
+                //Idoso
+                img.setAttribute('src','foto-idoso-m.jpeg')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if(idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute('src','foto-bb-f.jpeg')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src','foto-jovem-f.jpeg')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src','foto-adulto-f.jpeg')
+            } else {
+                //Idoso
+                img.setAttribute('src','foto-idoso-f.jpeg')
+            }
+        } 
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
         //13:30min aula 03
     }
 
